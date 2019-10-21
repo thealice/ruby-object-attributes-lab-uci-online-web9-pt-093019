3f9846b40713a7b4d21e2b897cf2fb976ed6afd1
Alice Freda <alicefreda@gmail.com>
2019-10-21 01:51:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,19 @@ class Dog
     @breed
   end
 end
+
+class Person
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def job=(job)
+    @job = job
+  end
+  def job
+    @job
+  end 
+
+end
